dd5836c36491eda19c9d3d7bb989259430eac5ab
doohinkus <doohinkus@gmail.com>
2018-02-22 11:27:34 -0800

    update gitignore

--- __tests__/components/notes.spec.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import Note from '../../src/components/notes';
-
-describe('edit', ()=>{
-  it('should set isEditing to true', ()=>{
-    expect(edit()).toEqual(true)
-  });
-});
